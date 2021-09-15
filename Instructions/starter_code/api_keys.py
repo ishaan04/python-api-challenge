fbf3179b1ee51bd2db2b4294047cdd80c532c5a3
ishaan04 <ishaan_nigam01@yahoo.com.au>
2021-09-16 00:44:52 +1000

    final push

--- Instructions/starter_code/api_keys.py
+++ Instructions/starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "f4243e09885630e9dced03c5ef1f7560"
+weather_api_key = "INSERT HERE"
 
 # Google API Key
-g_key = "AIzaSyCQfIxKFixL_6RMPpJBcR74Z1w6MJE15t4"
+g_key = "INSERT HERE"
